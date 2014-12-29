b1b761a95ad6a72ab39ce0d5be08d66f9f70afe3
Shuhel Ahmed <shuhel@ymail.com>
2014-12-29 16:01:53 +0600

    added can register and resolve transient

--- test/container.spec.js
+++ test/container.spec.js
@@ -1,4 +1,17 @@
 import {Container} from '../src/index';
+import {isClass} from '../src/util';
+
+class Calculator {
+    add(a, b){
+        return a + b;
+    }
+}
+
+class Screen {
+   display(content){
+     console.log(content);
+   }
+}
 
 describe('container', () => {
   it('should have some tests', () => {
@@ -6,8 +19,16 @@ describe('container', () => {
     expect(container).toBe(container);
   });
   
-  it('test is setup correctly', ()=> {
   
-    expect(true).toBe(false);
+  it('Can create transient instance', () => {
+    var container = new Container();
+    container.registerTransient(Calculator,Calculator);
+    var calc = container.get(Calculator);
+    expect(calc instanceof Calculator).toBe(true);
+  });
+
+  
+  it('test is setup correctly', ()=> {
+    expect(true).toBe(true);
   });
 });
